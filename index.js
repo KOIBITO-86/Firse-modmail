a645bf44d16c70e2c213829a19ed36f4c6d51309
Koibito <gyantidevi9878401132+1@gmail.com>
2024-10-09 12:18:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-Here's the modified code with the "Watching Tickets" status added:
-
-```
 const Discord = require('discord.js');
 const allIntents = new Discord.Intents(32767);
 const client = new Discord.Client({
